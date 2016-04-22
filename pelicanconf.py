70fabb562d89feac2dbbf0cf86d7989084e97205
Evandro Myller <eMyller@7ws.co>
2016-04-22 02:50:02 -0300

    Set up python-pelican to generate my blog

---
+++ pelicanconf.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Author info
+AUTHOR = 'Evandro Myller'
+SITENAME = 'emyller.github.io'
+SITEURL = ''
+
+# Content settings
+PATH = 'content'
+TIMEZONE = 'America/Sao_Paulo'
+DEFAULT_LANG = 'en'
+DEFAULT_PAGINATION = 10
+
+# Feeds
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = []
+
+# Social widget
+SOCIAL = []
